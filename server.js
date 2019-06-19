3d48f9b56a0dbc8db21bde1868fcd06f803c8892
chrismcglynn <chris@mcglynnenterprises.com>
2019-06-18 23:43:58 -0400

    route files with express router

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
-// Connect Database
+// Connect DB
 connectDB();
-
 app.get('/', (req, res) => res.send('API Running'));
 
-const PORT = process.env.PORT || 5000;
 
+// Define Routes
+app.use('/api/users', users);
+app.use('/api/auth', auth);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
+
+// Define PORT
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
